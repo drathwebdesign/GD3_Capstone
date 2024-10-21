cb96c8cbd68e77ae50a270c14625c3989bb75f61
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-21 16:07:49 +0100

    fix head bob

--- GD3_Capstone/Assets/Scripts/HeadBobController.cs
+++ GD3_Capstone/Assets/Scripts/HeadBobController.cs
@@ -24,7 +24,6 @@ public class HeadBobController : MonoBehaviour
 
         CheckMotion();
         ResetPosition();
-        _camera.LookAt(FocusTarget());
     }
     private void PlayMotion(Vector3 motion)
     {
