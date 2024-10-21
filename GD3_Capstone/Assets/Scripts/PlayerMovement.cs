169f0b7ff91ccd558e8b2d21be32834a05d1717c
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-21 14:49:52 +0100

    controller fixes

--- GD3_Capstone/Assets/Scripts/PlayerMovement.cs
+++ GD3_Capstone/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -6,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController characterController;
 
     [Header("Movement Variables")]
-    [SerializeField] float speed = 5f;
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float sprintSpeed = 7f;
     [SerializeField] float jumpHeight = 2f;
     [SerializeField] float gravity = -9.81f;
     [SerializeField] float gravityMultiplier = 2f;
@@ -17,11 +19,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] LayerMask ground;
 
     Vector3 velocity;
+    private float currentSpeed;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
 
+        currentSpeed = walkSpeed;
         //multiply gravity by multiplier
         gravity *= gravityMultiplier;
     }
@@ -32,8 +36,10 @@ public class PlayerMovement : MonoBehaviour
         GroundCheck();
         ApplyGravity();
         OnJump();
+        OnSprint();
         OnMove();
     }
+
     private void OnMove()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -41,21 +47,28 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * horizontal + transform.forward * vertical;
 
-        characterController.Move(move.normalized * speed * Time.deltaTime);
+        characterController.Move(move.normalized * currentSpeed * Time.deltaTime);
        
-        characterController.Move(velocity);
+        characterController.Move(velocity * Time.deltaTime);
+    }
+    private void OnSprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            currentSpeed = sprintSpeed;
+        else
+            currentSpeed = walkSpeed;
     }
     private void OnJump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity * Time.deltaTime);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
     }
     private void ApplyGravity()
     {
-        if (!isGrounded)
-            velocity.y += gravity * Mathf.Pow(Time.deltaTime, 2);
+        if (isGrounded && velocity.y < 0f)
+            velocity.y = -2.0f;
         else
-            velocity.y = 0;
+            velocity.y += gravity * Time.deltaTime;
     }
     private void GroundCheck()
     {
