cd66dde01c306f2334b1552dab4a4d693132079b
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-22 14:04:34 +0100

    footsteps

--- GD3_Capstone/Assets/FootStepsSystem.cs
+++ GD3_Capstone/Assets/FootStepsSystem.cs
@@ -7,22 +7,36 @@ public class FootStepsSystem : MonoBehaviour
 
     [Header("Audio")]
     [SerializeField] AudioSource playerAudioSource;
-    [SerializeField] AudioClip[] footstepSounds;
+    [SerializeField] AudioClip[] dirtFootStepsArray;
 
     private bool once = false;
-
+    private int previousIndex = 0;
     void LateUpdate()
     {
+        //if player is moving and camera is going down play sounds once per step
         if (headBobController.controller.GetMovementVector().magnitude > 0.1f && headBobController.GetCameraTransform().localPosition.y < 0)
         {
+            //use once to play sound once camera is negative
             if (!once)
             {
                 once = true;
-                int index = Random.Range(0, footstepSounds.Length);
-                playerAudioSource.PlayOneShot(footstepSounds[index]);
+                PlaySoundsBySurface();
             }
         }
+        //reset bool to play sound again
         else if (headBobController.GetCameraTransform().localPosition.y >= 0)
             once = false;
+    }
+    void PlaySoundsBySurface()
+    {
+        //get random index from array
+        int index = Random.Range(0, dirtFootStepsArray.Length);
+        previousIndex = index;
+        
+        //don't play same footstep twice
+        if (previousIndex == index && index < dirtFootStepsArray.Length - 1) 
+            index++;
+
+        playerAudioSource.PlayOneShot(dirtFootStepsArray[index]);
     }
 }
