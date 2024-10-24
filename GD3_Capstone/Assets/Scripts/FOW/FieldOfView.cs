83eee6240a17f12e77952023000728009cfa45de
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-24 15:45:31 +0100

    field of view

---
+++ GD3_Capstone/Assets/Scripts/FOW/FieldOfView.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new();
+
+    private void Start()
+    {
+        StartCoroutine(FindTargetsWithDelay(0.1f));
+    }
+    private IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+    private void FindVisibleTargets()
+    {
+        Collider[] targetsInRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInRadius.Length; i++)
+        {
+            Transform target = targetsInRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distance = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distance, obstacleMask))
+                {
+                    ////CAN SEE TARGET
+                    //add target to visible targets array
+                    visibleTargets.Add(target);
+
+                }
+                else
+                    visibleTargets.Remove(target);
+
+            }
+            else
+                visibleTargets.Remove(target);
+        }
+    }
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
