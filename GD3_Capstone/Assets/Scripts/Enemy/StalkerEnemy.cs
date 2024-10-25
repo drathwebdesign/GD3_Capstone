c1a2951a2e7ec81f2c515e482caac3e354937ae5
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-25 10:15:14 +0100

    inventory changes

--- GD3_Capstone/Assets/Scripts/Enemy/StalkerEnemy.cs
+++ GD3_Capstone/Assets/Scripts/Enemy/StalkerEnemy.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 public class StalkerEnemy : MonoBehaviour
 {
+    public float appereanceDelay = 1f;
+
+    [SerializeField] FieldOfView playerFow;
+    
+    private NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    public void MoveTowards()
+    {
+
+    }
     public void Appear()
     {
         gameObject.SetActive(true);
