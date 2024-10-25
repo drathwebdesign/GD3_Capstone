a41d0aae00895e8f15fd88a54b4e52e3ef0562aa
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-10-25 10:34:37 +0100

    some stuff

--- GD3_Capstone/Assets/Scripts/Enemy/StalkerEnemy.cs
+++ GD3_Capstone/Assets/Scripts/Enemy/StalkerEnemy.cs
@@ -4,7 +4,9 @@ using System.Collections;
 
 public class StalkerEnemy : MonoBehaviour
 {
+    public float attackRange = 10f;
     public float appereanceDelay = 1f;
+    public float positionOffsetToPlayer = 5f;
 
     [SerializeField] FieldOfView playerFow;
     
@@ -15,11 +17,28 @@ public class StalkerEnemy : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        
+        MovementCheck();
+        AttackCheck();
     }
-    public void MoveTowards()
+    public void MovementCheck()
     {
-
+        //if enemy is outside of player range move towards the player
+        if (Vector3.Distance(transform.position, playerFow.transform.position) > playerFow.viewRadius + positionOffsetToPlayer)
+        {
+            agent.Move(playerFow.transform.position);
+        }
+        //if enemy is too close to the player move him towards to scare player
+        else
+            agent.Move(playerFow.transform.forward * -1);
+    }
+    public void AttackCheck()
+    {
+        //if player gets in attack range attack the player and kill him
+        if(Vector3.Distance(transform.position, playerFow.transform.position) <= attackRange)
+        {
+            //waaaagh
+            agent.Move(playerFow.transform.position);
+        }
     }
     public void Appear()
     {
